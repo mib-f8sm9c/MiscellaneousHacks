4c730bb1a1049f91c4c868b9fbfc4e523b8b2271
Matthew McHaney <matthew.r.mchaney@gmail.com>
2015-07-26 11:49:58 -0500

    Put together a better version of the N64 Zelda Rom fixer ('fixes' errors from the Model2N64 program, though it might leave some triangles undrawn)

---
+++ ZeldaRomFixer/Zelda ROM Fixer/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ZeldaROMFixer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btnFix = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox.Location = new System.Drawing.Point(160, 28);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(413, 23);
+            this.textBox.TabIndex = 0;
+            // 
+            // btnFix
+            // 
+            this.btnFix.Enabled = false;
+            this.btnFix.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFix.Location = new System.Drawing.Point(117, 90);
+            this.btnFix.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFix.Name = "btnFix";
+            this.btnFix.Size = new System.Drawing.Size(365, 53);
+            this.btnFix.TabIndex = 1;
+            this.btnFix.Text = "Fix ROM";
+            this.btnFix.UseVisualStyleBackColor = true;
+            this.btnFix.Click += new System.EventHandler(this.btnFix_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(13, 21);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(119, 36);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load ROM...";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 156);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnFix);
+            this.Controls.Add(this.textBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = global::ZeldaROMFixer.Properties.Resources.triforce;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Zelda ROM Fixer V.0.2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btnFix;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
