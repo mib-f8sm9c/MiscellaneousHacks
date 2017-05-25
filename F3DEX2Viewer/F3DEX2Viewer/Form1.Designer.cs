106db8f88070baff185de7cb2d203821051ebdc9
mib_f8sm9c <mib.f8sm9c@gmail.com>
2017-05-24 21:02:09 -0500

    Added a F3DEX2Viewer, optimized for Mario Golf 64 viewing

---
+++ F3DEX2Viewer/F3DEX2Viewer/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace F3DEX2Viewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Cereal64.VisObj64.Visualization.OpenGL.Cameras.NewCamera newCamera1 = new Cereal64.VisObj64.Visualization.OpenGL.Cameras.NewCamera();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hexNumericUpDown1 = new Cereal64.Common.Controls.HexNumericUpDown();
+            this.openGLControl1 = new VisObj64.Visualization.OpenGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.hexNumericUpDown1);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.openGLControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(701, 309);
+            this.splitContainer1.SplitterDistance = 190;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Rom Dump Files|*.bin|All files|*.*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load File...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(42, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load Object";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Offset";
+            // 
+            // hexNumericUpDown1
+            // 
+            this.hexNumericUpDown1.Location = new System.Drawing.Point(27, 196);
+            this.hexNumericUpDown1.Maximum = new decimal(new int[] {
+            -1,
+            0,
+            0,
+            0});
+            this.hexNumericUpDown1.Minimum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.hexNumericUpDown1.Name = "hexNumericUpDown1";
+            this.hexNumericUpDown1.Size = new System.Drawing.Size(157, 26);
+            this.hexNumericUpDown1.State = Cereal64.Common.Controls.HexNumericUpDown.BaseState.hex;
+            this.hexNumericUpDown1.TabIndex = 3;
+            this.hexNumericUpDown1.Value = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.Camera = newCamera1;
+            this.openGLControl1.ClearColor = System.Drawing.Color.CornflowerBlue;
+            this.openGLControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLControl1.Location = new System.Drawing.Point(0, 0);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.Size = new System.Drawing.Size(507, 309);
+            this.openGLControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 309);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private VisObj64.Visualization.OpenGL.OpenGLControl openGLControl1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private Cereal64.Common.Controls.HexNumericUpDown hexNumericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ F3DEX2Viewer/F3DEX2Viewer/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Cereal64.Common.Rom;
+using Cereal64.Microcodes.F3DZEX;
+using Cereal64.VisObj64.Data.OpenGL.Wrappers.F3DZEX;
+using Cereal64.Microcodes.F3DZEX.DataElements;
+
+namespace F3DEX2Viewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //load the file
+                byte[] filedata = File.ReadAllBytes(openFileDialog1.FileName);
+
+                RomProject.Instance.AddRomFile(new RomFile("dumpFile", 0, new Cereal64.Common.DataElements.UnknownData(0, filedata)));
+                RomProject.Instance.AddDmaProfile(new DmaProfile("Default"));
+                DmaSegment seg =new DmaSegment();
+                seg.File = RomProject.Instance.Files[0];
+                seg.FileStartOffset = 0;
+                seg.FileEndOffset = seg.File.FileLength;
+                seg.RamSegment = 0x80;
+                seg.RamStartOffset = 0;
+                DmaSegment seg2 = new DmaSegment();
+                seg2.File = RomProject.Instance.Files[0];
+                seg2.FileStartOffset = 0x0;// 0x310000;
+                seg2.FileEndOffset = seg.File.FileLength;
+                seg2.RamSegment = 0x02;
+                seg2.RamStartOffset = 0x0;
+                RomProject.Instance.SelectedDmaProfile.RamSegments.Add(0x80, new List<DmaSegment>() { seg });
+                RomProject.Instance.SelectedDmaProfile.RamSegments.Add(0x02, new List<DmaSegment>() { seg2 });
+                
+                //perfect!
+
+                MessageBox.Show("File loaded successfully!");
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int offset = (int)hexNumericUpDown1.Value;
+
+            F3DZEXReaderPackage package = F3DZEXReader.ReadF3DZEXAt(RomProject.Instance.Files[0], offset);
+            F3DZEXCommandCollection commands = (F3DZEXCommandCollection)package.Elements[RomProject.Instance.Files[0]].FirstOrDefault(
+                c => c.FileOffset == offset && c is F3DZEXCommandCollection);
+
+            openGLControl1.GraphicsCollections.Clear();
+            openGLControl1.GraphicsCollections.Add(VO64F3DZEXReader.ReadCommands(commands, 0));
+
+            openGLControl1.RefreshGraphics();
+
+        }
+    }
+}
